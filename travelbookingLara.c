b3783a67afd91c676505d45ce6e1ad2b74dbadde
laraezz <97916338+laraezz@users.noreply.github.com>
2022-01-21 21:08:33 -0500

    Update and rename ezzeddineLaraA1.c to travelbookingLara.c

--- travelbookingLara.c
+++ travelbookingLara.c
@@ -1,14 +1,4 @@
-/************************EzzeddineLaraA1.c**************
-Student Name: Lara Ezzeddine Email Id: lezzeddi
-Due Date: 07/10/2020 Course Name: CIS 1300
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-1) I have read and understood the University policy on academic integrity;
-2) I have completed the Computing with Integrity Tutorial on Moodle; and
-3) I have achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and all material that I have used, whether directly quoted or paraphrased. Furthermore, I certify that this assignment was prepared by me specifically for this course.
-  ********************************************************/
-// compiling: gcc ezzeddineLaraA1.c -std=c99 -Wall
+// compiling: gcc travelbookingLara.c -std=c99 -Wall
 // running the program: ./a.out
 
 
